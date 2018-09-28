ae8d6d32451b2c8e4aa64bb0377b211f362e3f39
evgen10 <i.am.mr.jackson96@gmail.com>
2018-09-28 14:13:11 +0600

    first

---
+++ Part2.Test/Task2Test.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using EFModels.Model;
+using System.Linq;
+
+
+namespace Part2.Test
+{
+    [TestClass]
+    public class Task2Test
+    {
+        [TestMethod]
+        public void QueryDemonstration()
+        {
+            using (var db = new Northwind())
+            {
+                string categoryName = "Beverages";
+
+                var query = from order in db.Orders
+                            from orderDetails in order.Order_Details
+                            where orderDetails.Product.Category.CategoryName == categoryName
+                            select new
+                            {
+                                orderDetails.Product.Category.CategoryName,
+                                order.Customer.ContactName,
+                                orderDetails.Product.ProductName,
+                                order.OrderDate
+                            };
+    
+
+                #region Demonstration
+
+                Console.WriteLine(query.Count());
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine($" Category Name:  {item.CategoryName} Customer:  {item.ContactName} Product:  {item.ProductName} Date {item.OrderDate}");
+
+                }
+
+                #endregion
+
+               
+
+            }
+
+
+        }
+    }
+}
